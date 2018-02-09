f6aa881bee5cf3b7ee691d31c2fd5d396f9b34da
huoyu <fr440305@gmail.com>
2018-02-09 14:54:58 -0800

    ttt/game :: add code to check if no one wins

--- TicTacToe/Game.java
+++ TicTacToe/Game.java
@@ -7,27 +7,39 @@ public class Game {
 		this.grid = new Grid();
 		this.stepnum = 0;
 	}
-
 	public void Loop () {
 		char win_char;
 		int row;
 		int col;
 		String err;
 		Scanner ipt = new Scanner(System.in);
-		while ((win_char = this.grid.WhoWins()) == ' ') {
+		while (true) {
 			this.grid.Print();
+			// if win:
+			if ((win_char = this.grid.WhoWins()) != ' ') {
+				System.out.println(
+					"Player "+
+					win_char+
+					" wins on step "+
+					this.stepnum + "!"
+				);
+				return;
+			}
+			// if full:
+			if (this.grid.IsFull()) {
+				System.out.println("Grid Full. No one wins.");
+				return;
+			}
 			System.out.print("Player " + ((this.stepnum%2==0)?'O':'X') + " >>> ");
 			row = ipt.nextInt();
 			col = ipt.nextInt();
 			err = this.grid.Fill(row, col, (this.stepnum%2==0)?'O':'X');
-			if (!err.equals("")) {
+			if (err.length() != 0) {
 				System.out.println(err);
 				System.out.println("Try again!");
 				continue;
 			}
 			this.stepnum += 1;
 		}
-		this.grid.Print();
-		System.out.println("Player " + win_char + " wins on step " + this.stepnum + "!");
 	}
 }
