8746d4dfd8c07d4092254db4aae629427b5e7798
huoyu <fr440305@gmail.com>
2018-02-09 14:57:27 -0800

    ttt/main :: bugfix: if player press enter without input anything...

--- TicTacToe/Main.java
+++ TicTacToe/Main.java
@@ -5,10 +5,13 @@ public class Main {
 	public static void main (String[] args) {
 		while (true) {
 			game = new Game();
+			String again;
 			game.Loop(); // blocking....
 			System.out.println("Do you want to play it again? (y/n) : ");
-			switch ((new Scanner(System.in)).nextLine().charAt(0)) {
-				case 'n': case 'N': return;
+			if ((again = (new Scanner(System.in)).nextLine()).length() != 0) {
+				switch (again.charAt(0)) {
+					case 'n': case 'N': return;
+				}
 			}
 		}
 	}
